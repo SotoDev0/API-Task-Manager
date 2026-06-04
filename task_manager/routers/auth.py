6c560940bd4b183a0422865423846904214dcb10
SotoDev0 <javier.soto8934@gmail.com>
2026-06-04 19:55:32 -0400

    add JWT authentication

---
+++ task_manager/routers/auth.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from .. import models, schemas
+from .. database import get_db
+from ..core.security import hash_password, verify_password, create_access_token
+
+router = APIRouter(prefix="/auth", tags=["auth"])
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    from ..core.security import decode_token
+    username = decode_token(token)
+    if not username:
+        raise HTTPException(status_code=401, detail="Token invalido")
+    user = db.query(models.user).filter(models.User.username == username).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="Usuario no encontrado")
+    return user
+
+@router.post("/register", response_model=schemas.UserOut)
+def register(user: schemas.UserCreate, db: Session=Depends(get_db)):
+    existing = db.query(models.User).filter(models.User.username == user.username).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="El usuario ya existe")
+    
+    new_user = models.User(
+        username = user.username,
+        email = user.email,
+        hash_password = hash_password(user.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@router.post("login", response_model=schemas.Token)
+def login(form: OAuth2PasswordRequestForm = Depends(), db:Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.username == form.username).first()
+    if not user or not verify_password(form.password, user.hashed_password()):
+        raise HTTPException(status_code=401, detail="credenciales incorrectas")
+    
+    token = create_access_token(data={"sub":user.username})
+    return {"access_token": token, "token_type" : "bearer"}
